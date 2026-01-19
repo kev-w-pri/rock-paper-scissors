123eab023cd08c32f0c00cb4bcc2f382da3027cf
kev-w-pri <kev.w.pri@gmail.com>
2026-01-19 12:37:46 +0100

    Copmleted assignment, forgot to commit more often

--- javascript.js
+++ javascript.js
@@ -1,1 +1,56 @@
-console.log("Hello, World!");
+function playGame(){
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    let rounds = 1;
+
+    function playRound(humanChoice, computerChoice){
+
+        function getComputerChoice(){
+
+            let randomNumber = Math.floor(Math.random() * 100);
+            if (randomNumber <= 33){
+                return "rock";
+            } else if (33 < randomNumber && randomNumber <= 66){
+                return "paper";
+            } else if (66 < randomNumber && randomNumber <= 99){
+                return "scissors";
+            }
+        }
+
+        function getHumanChoice(){
+            let choice = prompt("Round " + rounds + ":" + " Choose your fighter (Rock, Paper, Scissors):").toLowerCase();
+            return choice;
+        }
+
+        const humanThrow = getHumanChoice();
+        const computerThrow = getComputerChoice();
+
+
+        console.log(computerThrow);
+        console.log(humanThrow);
+
+        if (humanThrow == computerThrow){
+            return console.log("It's a draw! " + humanThrow + " and " + computerThrow + " are the same." + " That's " + humanScore + " wins for you and " + computerScore + " wins for the robot.");
+
+        } else if((humanThrow == "rock" && computerThrow == "scissors") || (humanThrow == "scissors" && computerThrow == "paper") || (humanThrow == "paper" && computerThrow == "rock")){
+            humanScore++;
+            return console.log("You win! " + humanThrow + " beats " + computerThrow + ". That's " + humanScore + " wins.");
+
+        } else{
+            computerScore++;
+            return console.log("Sorry, loser! " + computerThrow + " beats " + humanThrow + " The robot has " + computerScore + " wins now. Are you proud?");
+        }
+    }
+
+    while (rounds <= 5) {
+        playRound();
+        rounds++;
+    }
+
+
+   
+}
+
+playGame();
